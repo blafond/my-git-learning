8b03cd606e279e9a7b21fadb4d99d54210f5f429
Barry LaFond <blafond@redhat.com>
2023-05-25 15:36:52 -0500

    [#3] 6th project commit

--- learning-core/src/main/java/org/hibernate/learning/PrintClass.java
+++ learning-core/src/main/java/org/hibernate/learning/PrintClass.java
@@ -10,12 +10,15 @@ public class PrintClass {
 	}
 
 	public void printIntegers( int start, int howMany) {
+		logToSystem( "\nSTART = " + start + " How many = " + howMany );
 		for( int i = start; i< howMany; i++) {
 			System.out.println("\t" + i);
 		}
+
 	}
 
 	public void printIntegersWithMultiplier( int start, int howMany, int multiplier) {
+		logToSystem( "\nSTART = " + start + " How many = " + howMany + " Multiplier = " + multiplier);
 		for( int i = start; i< howMany; i++) {
 			System.out.println("\t" + i*multiplier);
 		}
