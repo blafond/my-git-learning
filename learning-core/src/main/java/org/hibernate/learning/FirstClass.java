de4110baa7f9bdbcf7b37cf948e820894afedfd9
Barry LaFond <blafond@redhat.com>
2023-05-25 14:00:50 -0500

    [#2] second project commit

--- learning-core/src/main/java/org/hibernate/learning/FirstClass.java
+++ learning-core/src/main/java/org/hibernate/learning/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		PrintClass printMsg = new PrintClass();
 		String msg = " >>> FirstClass.java class :  main()";
-		printMsg.logToSystem( msg );
+		printMsg.logToSystem( msg, "\t\tSTARTED" );
+		printMsg.logToSystem( msg, "\t\tCOMPLETED!!!!" );
 	}
 }
--- learning-core/src/main/java/org/hibernate/learning/PrintClass.java
+++ learning-core/src/main/java/org/hibernate/learning/PrintClass.java
@@ -4,4 +4,8 @@ public class PrintClass {
 	public void logToSystem(String msg) {
 		System.out.println(msg);
 	}
+
+	public void logToSystem(String msg, String anotherMsg) {
+		System.out.println(msg + "\n" + anotherMsg);
+	}
 }
